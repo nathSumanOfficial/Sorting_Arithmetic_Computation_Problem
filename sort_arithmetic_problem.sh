ab91e72eb3c7875c58d099b48410c069a9d25488
Suman Nath <nathsuman1099@gmail.com>
2021-06-01 19:31:39 +0530

    Store values from dictionary to array

--- sort_arithmetic_problem.sh
+++ sort_arithmetic_problem.sh
@@ -8,22 +8,25 @@ read b
 read c 
 
 declare -A resultDict
+arr=()
 
 for (( i = 0; i < 4; i++ )); do
 	case $i in
 		0)	result=`echo | awk "{print $a+$b*$c}"`
-			resultDict[$i]=$result
+			resultDict[$i]=$result 
 			;;
 		1)	result=`echo | awk "{print $a*$b+$c}"`
-			resultDict[$i]=$result
+			resultDict[$i]=$result 
 			;;
 		2)	result=`echo | awk "{print $c+$a/$b}"`
-			resultDict[$i]=$result
+			resultDict[$i]=$result 
 			;;
 		3)	result=`echo | awk "{print $a%$b+$c}"`
-			resultDict[$i]=$result
+			resultDict[$i]=$result 
 			;;
 	esac
 done
 
-echo ${resultDict[@]}
+for (( i = 0; i < 4; i++ )); do
+	arr[$i]=${resultDict[$i]}
+done
